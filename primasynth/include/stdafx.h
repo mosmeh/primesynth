152789db1fb39cc88b54a1d1285a9a4416f63fbc
mosm <airman515@gmail.com>
2018-06-08 05:44:32 +0900

    Create src, include and lib directories

---
+++ primasynth/include/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define NOMINMAX
+#include <Windows.h>
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <array>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+#include "portaudio.h"
